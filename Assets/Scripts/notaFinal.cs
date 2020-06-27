dae13c05bcb78833066fcbeed1b03bcde159d70b
Karina Pereira <kapcruz@gmail.com>
2020-06-27 19:15:28 -0300

    Colocando questoes para a primeira categoria e respostas faltando nota final

---
+++ Assets/Scripts/notaFinal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class notaFinal : MonoBehaviour
+{
+
+    private int idCategoria;
+
+    public Text txtNota;
+    public Text txtInfoCategoria;
+
+    public GameObject trofeu1;
+    public GameObject trofeu2;
+    public GameObject trofeu3;
+
+    private int notaTotal;
+    private int idAcerto;
+
+
+    void Start()
+    {
+        idCategoria = PlayerPrefs.GetInt("idCategoria");
+        notaTotal = PlayerPrefs.GetInt("notaFinalTemp"+idCategoria.ToString());
+        idAcerto = PlayerPrefs.GetInt("acertosTemp" + idCategoria.ToString());
+
+
+        txtNota.text = notaTotal.ToString();
+        txtInfoCategoria.text = "Voce acertou " + idAcerto.ToString() + " de 10 perguntas";
+    }
+}
---
+++ Assets/Scripts/response.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class response : MonoBehaviour
+{
+
+    public int idCategoria;
+
+    public Text pergunta;
+    public Text respostaA;
+    public Text respostaB;
+    public Text respostaC;
+    public Text respostaD;
+    public Text infoResposta;
+
+    // array que guarda todas as perguntas, respostas e acertos 
+    public string[] perguntas;
+    public string[] questaoA;
+    public string[] questaoB;
+    public string[] questaoC;
+    public string[] questaoD;
+    public string[] acertos;
+
+    private int idPergunta;
+    private float idAcerto;
+    private float idQuestao;
+    private float mediaAcertos;
+    private int notaFinal;
+
+
+    void Start() 
+    {
+        idCategoria = PlayerPrefs.GetInt("idCategoria");
+        idPergunta = 0;
+        idQuestao = perguntas.Length;
+
+        pergunta.text = perguntas[idPergunta];
+        respostaA.text = questaoA[idPergunta];
+        respostaB.text = questaoB[idPergunta];
+        respostaC.text = questaoC[idPergunta];
+
+        infoResposta.text = "Respondendo " + (idCategoria + 1).ToString() + " de " + idQuestao.ToString() + " questões";
+
+    }
+
+    public void resposta(string questao)
+    {
+        if(questao == "A")
+        {
+            if (questaoA[idPergunta] == acertos[idPergunta])
+            {
+                idAcerto += 1;
+            }
+
+        }
+        else if(questao == "B")
+        {
+
+            if (questaoB[idPergunta] == acertos[idPergunta])
+            {
+                idAcerto += 1;
+            }
+        }
+        else if (questao == "C")
+        {
+            if (questaoC[idPergunta] == acertos[idPergunta])
+            {
+                idAcerto += 1;
+            }
+
+        }
+        else if(questao == "D")
+        {
+
+            if (questaoD[idPergunta] == acertos[idPergunta])
+            {
+                idAcerto += 1;
+            }
+
+        }
+
+        proximaQuestao();
+    }
+
+    public void proximaQuestao()
+    {
+
+        idPergunta += 1;
+
+        if (idPergunta <= (idQuestao - 1))
+        {
+
+            pergunta.text = perguntas[idPergunta];
+            respostaA.text = questaoA[idPergunta];
+            respostaB.text = questaoB[idPergunta];
+            respostaC.text = questaoC[idPergunta];
+            respostaD.text = questaoD[idPergunta];
+
+            infoResposta.text = "Respondendo " + (idCategoria + 1).ToString() + " de " + idQuestao.ToString() + " questões";
+        }
+        else
+        {
+            mediaAcertos = 10 * (idAcerto / idQuestao);
+            notaFinal = Mathf.RoundToInt(mediaAcertos); // responsável por arredondar a nota para inteiro
+
+            if (notaFinal > PlayerPrefs.GetInt("notaFinal" + idCategoria.ToString()))
+            {
+                PlayerPrefs.SetInt("notaFinal" + idCategoria.ToString(), notaFinal);
+                PlayerPrefs.SetInt("acertos" + idCategoria.ToString(), (int)idAcerto);
+            }
+
+            PlayerPrefs.SetInt("notaFinalTemp" + idCategoria.ToString(), notaFinal);
+            PlayerPrefs.SetInt("acertosTemp" + idCategoria.ToString(), (int)idAcerto);
+
+            SceneManager.LoadScene("nota");
+        }
+
+
+    }
+}
