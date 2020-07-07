8564979ac0e4d677efea64ed10a7b44f2d360dd2
Karina Pereira <kapcruz@gmail.com>
2020-07-07 01:38:03 -0300

    Adicionando mais perguntas e respostas e animacao na categoria 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horizontal = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+
+
+    }
+}
